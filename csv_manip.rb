516dc9adf31c0a68da03030ae1dea256813ac401
brent <brent.wish@gmail.com>
2015-08-05 20:04:26 -0700

    Creates Google Calendar events for each workday given a name and a schedule

--- csv_manip.rb
+++ csv_manip.rb
@@ -1,8 +1,8 @@
+require './calendar_event_creator.rb'
 require 'csv'
 require 'date'
 
-NAME = "brent"
-days = [
+DAYS = [
   {:day => "MONDAY", :name => 1, :hours => 2 },
   {:day => "TUESDAY", :name => 3, :hours => 4 },
   {:day => "WEDNESDAY", :name => 5, :hours => 6 },
@@ -11,21 +11,16 @@ days = [
   {:day => "SATURDAY", :name => 11, :hours => 12 },
   {:day => "SUNDAY", :name => 13, :hours => 14 }]
 
-schedule = CSV.read("july_27_2015.csv")
+@schedule = CSV.read("july_27_2015.csv")
+@person = "brent"
 
 #Takes the schedule and returns an array
 #that (hopefully) contains the two rows
 #that the dates are on.
-def get_biweek(schedule)
+def get_biweek()
   a = []
-  week_days = ["MONDAY",
-               "TUESDAY",
-               "WEDNESDAY",
-               "THURSDAY",
-               "FRIDAY",
-               "SATURDAY",
-               "SUNDAY"]
-  schedule.each_with_index do |row, i|
+  week_days = DAYS.map { |day| day[:day] }
+  @schedule.each_with_index do |row, i|
     row = row.compact.map { |a| a.upcase }
     if row.all? { |word| week_days.include?(word) } && row != []
       a.push(i + 1)
@@ -36,7 +31,7 @@ end
 
 #Takes a day and hour and formats it as a date
 def get_date(day, hours)
-  current_time = Time.new(2015, 7, 26) #Preset date for testing. Should be current time
+  current_time = Time.new(2015, 7, 12) #Preset date for testing. Should be current time
   if day.to_i > current_time.day
     month = current_time.month
     year = current_time.year
@@ -49,26 +44,32 @@ def get_date(day, hours)
       year = current_time.year
     end
   end
-  return Time.new(year, month, day, hours)
+  year = year.to_i
+  month = month.to_i
+  day = day.to_i
+  hours = hours[0].to_i + 12
+  start_time = DateTime.new(year, month, day, hours, 0, 0, '-7').to_s
+  end_time = DateTime.new(year, month, day, 18, 0, 0, '-7').to_s
+  return { :start => start_time, :end => end_time }
 end
 
 #takes the schedule, days, and a person and returns
 #an array of work days that that name works on
-def package_date_time(schedule, days, person)
+def get_work_days
   work_days = []
-  biweek = get_biweek(schedule)
-  week_1 = schedule[biweek[0]].compact.uniq
-  week_2 = schedule[biweek[1]].compact.uniq
+  biweek = get_biweek
+  week_1 = @schedule[biweek[0]].compact.uniq
+  week_2 = @schedule[biweek[1]].compact.uniq
 
-  schedule.each_with_index do |row, i|
-    days.each_with_index do |day, j|
+  @schedule.each_with_index do |row, i|
+    DAYS.each_with_index do |day, j|
       date = ( i < biweek[1] ? week_1[j] : week_2[j] )
       day_name = day[:day]
       name = row[day[:name]]
       hours = row[day[:hours]]
-      if name && hours && name.downcase.include?(person)
+      if name && hours && name.downcase.include?(@person.downcase)
         work_days.push({
-          :name => person,
+          :name => @person,
           :date => get_date(date, hours)
         })
       end
@@ -77,4 +78,16 @@ def package_date_time(schedule, days, person)
   return work_days
 end
 
-puts package_date_time(schedule, days, NAME)
+def set_schedule
+  work_days = get_work_days
+  work_days.each do |day|
+    start_time = day[:date][:start]
+    end_time = day[:date][:end]
+    CalendarEventCreator.create_event(start_time, end_time)
+  end
+  CalendarEventCreator.fetch_ten_events
+end
+
+CalendarEventCreator.init
+set_schedule
+puts get_work_days
